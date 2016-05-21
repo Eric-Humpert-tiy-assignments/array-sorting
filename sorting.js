489a48d0b5b949e82d4a00b43519dca3731a7365
Thwipshot <ethumpert@gmail.com>
2016-05-21 10:56:00 -0500

    completed the sorting functions for the assignment

--- sorting.js
+++ sorting.js
@@ -1,6 +1,7 @@
 
 function sortFood() {
-  var food = ['steak', 'shrimp', 'apples'].sort();
+  var food = ['steak', 'shrimp', 'apples']
+    return food.sort();
 
   //implement
 }
@@ -17,12 +18,24 @@ var people = [
 
 function sortPeopleByAgeAscending() {
   //implement
+  return people.sort(function(a, b) {
+    if (a.age > b.age) {
+      return 1;
+    }
+  })
 }
 
 console.log('sort by age', sortPeopleByAgeAscending()); // -> Dirk, then Naaman, then Bob, then Sue, and finally George
 
 function sortPeopleByNameDescending() {
   //implement
+  return people.sort(function(a, b) {
+    var nameA = a.name.toUpperCase();
+    var nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return 1;
+    }
+  })
 }
 
 console.log('sort by name', sortPeopleByNameDescending()); // -> Sue, then Naaman, then George, then Dirk, and finally Bob
